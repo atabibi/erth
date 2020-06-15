1f91848740a8584712c23756913ee84a11336cb6
Abbas Tabibi <tabibi.abbas@gmail.com>
2020-06-15 09:14:19 +0430

    ایجاد کامپوننت RecordNavigation

---
+++ Erth.Client/Models/RecordNavigationEventArgs.cs
@@ -0,0 +1,10 @@
+namespace Erth.Client.Models
+{
+    public class RecordNavigationEventArgs
+    {
+        public int FromIndex { get; set; }
+        public int Count { get; set; }
+        public int AllRecordsCount { get; set; }
+        
+    }
+}
