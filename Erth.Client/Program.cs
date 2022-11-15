15c532d5ed5a6a2d9623e00d72029c291e67f193
Abbas Tabibi <tabibi.abbas@gmail.com>
2022-11-15 20:24:01 +0330

    Upgrade to .Net 6.0

--- Erth.Client/Program.cs
+++ Erth.Client/Program.cs
@@ -1,33 +1,31 @@
-﻿using Erth.Client.Services.Contracts;
+﻿using Erth.Client;
+using Erth.Client.Services.Contracts;
 using Erth.Client.Services.Implementations;
 using Erth.Client.States;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Erth.Client
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddOptions();
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
-            builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
-            builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//namespace Erth.Client;
 
 
-            var host = builder.Build();
-            await host.RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+var host = builder.Build();
+await host.RunAsync();
+    
